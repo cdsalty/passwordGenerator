b20f5a732a99c8565bb85868e5a8d982c3bebc6a
cdsalty <soltiscd@gmail.com>
2019-12-23 23:59:36 -0500

    generating password in process

--- passGen.js
+++ passGen.js
@@ -1,14 +1,29 @@
-console.log("sanity-check");
-
-// Step 1: Connect slider with the input entered (set characterAmountRange to characterAmountNumber)
 const characterAmountRange = document.getElementById("characterAmountRange");
 const characterAmountNumber = document.getElementById("characterAmountNumber");
+const form = document.getElementById("passwordGeneratorForm");
+const includeUppercaseElement = document.getElementById("includeUppercase");
 
 characterAmountNumber.addEventListener("input", syncTogether);
 characterAmountRange.addEventListener("input", syncTogether);
+passwordGeneratorForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const characterAmount = characterAmountNumber.value;
+  const includeUppercase = includeUppercaseElement.checked; // returns true or false if the button is checked.
+  // const includeNumbers =
+  // const includeSymbols =
+  // get a password variable from a function
+  const password = generatePassword(
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+});
 
 function syncTogether(e) {
   const inputValue = e.target.value;
   characterAmountNumber.value = inputValue;
   characterAmountRange.value = inputValue;
 }
+
+// 2. Setup the form so when user selects "generate a password," it returns a password instead of refreshing page
--- passGenNotes.js
+++ passGenNotes.js
@@ -15,3 +15,21 @@ function syncTogether(e) {
   characterAmountRange.value = inputValue;
 }
 //  _________________________________________________________________________________________________________________________________
+// // 2. Setup the form so when user selects "generate a password," it returns a password instead of refreshing page
+const form = document.getElementById("passwordGeneratorForm");
+const includeUppercaseElement = document.getElementById("includeUppercase");
+
+passwordGeneratorForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const characterAmount = characterAmountNumber.value;
+  const includeUppercase = includeUppercaseElement.checked; // returns true or false if the button is checked.
+  // const includeNumbers =
+  // const includeSymbols =
+  // get a password variable from a function
+  const password = generatePassword(
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+});
